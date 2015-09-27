0b89fad5f5a9431e6e45f5a235100c95af85378f
Lars Thorup <lars@zealake.com>
2015-09-27 11:04:14 +0200

    react class

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
+var Profile = React.createClass({
+  propTypes: {
+    role: React.PropTypes.string.isRequired,
+    url: React.PropTypes.string.isRequired,
+    text: React.PropTypes.string.isRequired
+  },
+
+  render: function () {
+    return React.createElement('li', {className: 'Profile'},
+      React.createElement('h2', {className: 'Profile-role'}, this.props.role),
+      React.createElement('a', {href: this.props.url}, this.props.text)
+    )
+  }
+});
+
+var profiles = [
+  {key: 'twitter', role: 'News', url: 'https://twitter.com/larsthorup', text: '@larsthorup'},
+  {key: 'tumblr', role: 'Photos', url: 'http://larsthorup.tumblr.com', text: 'larsthorup.tumblr.com'},
+];
+
+
 var rootElement =
   React.createElement('div', {},
-    React.createElement('h1', {}, 'Contacts'),
+    React.createElement('h1', {}, 'Profiles'),
     React.createElement('ul', {},
-      React.createElement('li', {},
-        React.createElement('h2', {}, 'Twitter'),
-        React.createElement('a', {href: 'https://twitter.com/larsthorup'}, 'twitter.com/larsthorup')
-      ),
-      React.createElement('li', {},
-        React.createElement('h2', {}, 'Tumblr'),
-        React.createElement('a', {href: 'http://larsthorup.tumblr.com'}, 'larsthorup.tumblr.com')
-      )
+      profiles.map(profile => React.createElement(Profile, profile))
     )
   );
 
